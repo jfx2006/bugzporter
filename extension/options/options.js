bfc13cea358ecee3c1624f00dd4d616ea118e465
Rob Lemley <rob@thunderbird.net>
2023-01-30 11:45:06 -0500

    Turn save button green to indicate saved value

--- extension/options/options.js
+++ extension/options/options.js
@@ -3,7 +3,9 @@ document.querySelector("form").onsubmit = async (event) => {
   browser.storage.local.set({
     apiToken: document.querySelector("#apiToken").value,
   })
-  document.querySelector("button").textContent = "Saved"
+  const btn = document.querySelector("button")
+  btn.textContent = "Saved"
+  btn.classList.add("saved")
 }
 
 browser.storage.local.get("apiToken", ({ apiToken }) => {
